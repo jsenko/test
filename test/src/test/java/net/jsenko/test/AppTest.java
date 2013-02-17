a341427c3600eecdfff686a8d2579b0592da3d88
Jakub Senko <jsenko@redhat.com>
2013-02-17 17:38:01 +0100

    Initial commit.

---
+++ test/src/test/java/net/jsenko/test/AppTest.java
@@ -0,0 +1,20 @@
+package net.jsenko.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue(true);
+    }
+}
